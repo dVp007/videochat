87cbc5d3e905e2937df93adda640738390bb66f5
dharmil <dharmilp03@gmail.com>
2020-08-25 18:02:22 +0530

    update

--- server.js
+++ server.js
@@ -1,83 +1,88 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const path = require("path");
-var cors = require("cors");
-const socket = require("socket.io");
-const { v4: uuidv4 } = require("uuid");
-const AccessToken = require("twilio").jwt.AccessToken;
+const path = require('path');
+var cors = require('cors');
+const socket = require('socket.io');
+const {
+  v4: uuidv4
+} = require('uuid');
+const AccessToken = require('twilio').jwt.AccessToken;
 const VideoGrant = AccessToken.VideoGrant;
-require("dotenv").config();
+require('dotenv').config();
 
 const MAX_ALLOWED_SESSION_DURATION = 14400;
 const twilioAccountSid = process.env.TWILIO_ACCOUNT_SID;
 const twilioApiKeySID = process.env.TWILIO_API_KEY_SID;
 const twilioApiKeySecret = process.env.TWILIO_API_KEY_SECRET;
 
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(path.join(__dirname, 'build')));
 app.use(cors());
 
 var activeUsers = [];
 
-app.get("/token", (req, res) => {
-  console.log("accountSID:", twilioAccountSid);
-  console.log("API KEY:", twilioApiKeySID);
-  console.log("API Secret:", twilioApiKeySecret);
-  const { identity, roomName } = req.query;
+app.get('/token', (req, res) => {
+  console.log('accountSID:', twilioAccountSid);
+  console.log('API KEY:', twilioApiKeySID);
+  console.log('API Secret:', twilioApiKeySecret);
+  const {
+    identity,
+    roomName
+  } = req.query;
   const token = new AccessToken(
-    "AC07deb1575a8c94e295ae46fb2c31e86e",
-    "SK11e2a6d6d727d4790255e32ec03ba750",
-    "OE9KauaT8qONjuX7HCs2vrpk4YDTzcr1",
-    {
+    'AC07deb1575a8c94e295ae46fb2c31e86e',
+    'SK11e2a6d6d727d4790255e32ec03ba750',
+    'OE9KauaT8qONjuX7HCs2vrpk4YDTzcr1', {
       ttl: MAX_ALLOWED_SESSION_DURATION,
     }
   );
   token.identity = identity;
   const videoGrant = new VideoGrant({
-    room: roomName,
+    room: roomName
   });
   token.addGrant(videoGrant);
   res.status(200).send({
-    token: token.toJwt(),
+    token: token.toJwt()
   });
   console.log(`issued token for ${identity} in room ${roomName}`);
 });
 
-app.get("*", (_, res) =>
-  res.sendFile(path.join(__dirname, "build/index.html"))
-);
+app.get('*', (_, res) => res.sendFile(path.join(__dirname, 'build/index.html')));
 
-var server = app.listen(process.env.PORT, () =>
-  console.log("token server running on ")
-);
+
+var server = app.listen(process.env.PORT, () => console.log('token server running on '));
 const io = socket(server);
-io.on("connection", async function (socket) {
-  console.log("Made a socket connection");
-  socket.on("new-user", function (data) {
-    console.log(data);
+io.on('connection', async function (socket) {
+  console.log('Made a socket connection');
+  socket.on('new-user', function (data) {
+    console.log(data)
     activeUsers.push({
       username: data,
-      id: socket.id,
+      id: socket.id
     });
-    io.emit("new-users", [...activeUsers]);
+    io.emit('new-users', [...activeUsers]);
   });
-  socket.on("sendMessage", function (data) {
-    console.log(data);
-    socket.broadcast.to(data.id).emit(
-      "recieveMessage",
-      JSON.stringify({
-        roomName: data.roomName,
-        username: data.username,
-      })
-    );
+
+  socket.on('cancelCall', function (data) {
+    console.log('canceledData:', data)
+    socket.broadcast.to(data.id).emit('callcanceled', data.roomName)
+  })
+
+  socket.on('sendMessage', function (data) {
+    console.log(data)
+    socket.broadcast.to(data.id).emit('recieveMessage', JSON.stringify({
+      id: data.id,
+      roomName: data.roomName,
+      username: data.username
+    }));
   });
-  socket.on("room-connected", function (data) {
-    console.log(data);
-    socket.broadcast.to(data.userId).emit("connectToRoom", data.username);
-  });
-  socket.on("disconnect", function () {
-    console.log("Socket :", socket.id);
-    activeUsers = activeUsers.filter((e) => e.id !== socket.id);
-    console.log(activeUsers);
-    io.emit("User disconnected", socket.userId);
+  socket.on('room-connected', function (data) {
+    console.log(data)
+    socket.broadcast.to(data.userId).emit('connectToRoom', data.username)
+  })
+  socket.on('disconnect', function () {
+    console.log('Socket :', socket.id)
+    activeUsers = activeUsers.filter(e => e.id !== socket.id)
+    console.log(activeUsers)
+    io.emit('User disconnected', socket.userId);
   });
 });
